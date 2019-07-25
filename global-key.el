8ac89fad93a071136da7bbf6ad11b42e0ce966a5
Nguyen Duc Chung <nguyenducchung@sutunam.com>
2019-07-25 14:52:41 +0700

    My very first emacs config

---
+++ global-key.el
@@ -0,0 +1,9 @@
+(cua-mode t)
+
+;; Separate C-i and "Tab" key
+;; Source: https://stackoverflow.com/a/11319885/6881855
+(define-key input-decode-map (kbd "C-i") (kbd "H-i"))
+(global-set-key (kbd "H-i") 'previous-line)
+(global-set-key (kbd "C-k") 'next-line)
+(global-set-key (kbd "C-j") 'backward-char)
+(global-set-key (kbd "C-l") 'forward-char)
---
+++ init.el
@@ -0,0 +1,33 @@
+;; Define load custom .el file function
+;; Source: https://stackoverflow.com/a/2079146/6881855
+(defconst user-init-dir
+  (cond ((boundp 'user-emacs-directory)
+         user-emacs-directory)
+        ((boundp 'user-init-directory)
+         user-init-directory)
+        (t "~/.emacs.d/")))
+(defun load-user-file (file)
+  (interactive "f")
+  "Load a file in current user's configuration directory"
+  (load-file (expand-file-name file user-init-dir)))
+
+;; Require Emacs' package functionality
+(require 'package)
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("gnu" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+
+;;(require 'graphene)
+
+(load-user-file "global-key.el")
