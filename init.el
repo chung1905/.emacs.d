8146bb34d030f8ac80a6aac15267bb10ee058b18
Nguyen Duc Chung <nguyenducchung@sutunam.com>
2019-07-29 10:05:02 +0700

    use-package is good

--- init.el
+++ init.el
@@ -13,9 +13,11 @@
 
 ;; Require Emacs' package functionality
 (require 'package)
+
 (setq package-archives
       '(("melpa" . "https://melpa.org/packages/")
-        ("gnu" . "https://elpa.gnu.org/packages/")))
+     	 ("gnu" . "https://elpa.gnu.org/packages/")
+	     ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
 
 (unless package-archive-contents
@@ -27,7 +29,20 @@
 (eval-when-compile
   (require 'use-package))
 
-
-;;(require 'graphene)
+(use-package graphene
+  :ensure t)
 
 (load-user-file "global-key.el")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (company-php graphene use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
