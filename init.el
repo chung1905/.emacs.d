41e2c69372ed01d602022dde4ecb0c80d51efff9
Nguyen Duc Chung <nguyenducchung@sutunam.com>
2019-08-06 18:04:54 +0700

    Yes, ac-php works

--- init.el
+++ init.el
@@ -6,6 +6,10 @@
   "Load a file in current user's configuration directory"
   (load-file (expand-file-name file user-init-dir)))
 
+(fset 'yes-or-no-p 'y-or-n-p)
+(toggle-frame-maximized)
+(menu-bar-mode 0)
+
 ;; Require Emacs' package functionality
 (require 'package)
 (setq package-archives
@@ -22,6 +26,57 @@
 
 (eval-when-compile
   (require 'use-package))
+
+(use-package php-mode
+  :ensure t)
+
+;;(use-package counsel
+;;  :ensure t
+;;  :config
+;;  (ivy-mode 1))
+
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-mode +1)
+  :bind-keymap
+  ("C-c p" . projectile-command-map))
+
+(use-package yasnippet
+  :ensure t
+  :diminish yas-minor-mode
+  :config
+  (yas-global-mode 1))
+
+(use-package ac-php
+  :ensure t)
+
+(use-package flycheck
+  :ensure t
+  :config
+  (global-flycheck-mode t))
+
+(add-hook 'php-mode-hook
+          '(lambda ()
+             ;; Enable auto-complete-mode
+             (auto-complete-mode t)
+
+             (require 'ac-php)
+             (setq ac-sources '(ac-source-php))
+
+             ;; As an example (optional)
+;;             (yas-global-mode 1)
+
+             ;; Enable ElDoc support (optional)
+             (ac-php-core-eldoc-setup)
+
+             ;; Jump to definition (optional)
+             (define-key php-mode-map (kbd "M-]")
+               'ac-php-find-symbol-at-point)
+
+             ;; Return back (optional)
+             (define-key php-mode-map (kbd "M-[")
+               'ac-php-location-stack-back)))
 
 (load-user-file "global-key.el")
 
