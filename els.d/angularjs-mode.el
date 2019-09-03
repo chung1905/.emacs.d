bf111aafa999d6b14715fa35addeea014287afcd
Nguyen Duc Chung <nguyenducchung@sutunam.com>
2019-09-03 09:51:20 +0700

    AngularJs

---
+++ els.d/angularjs-mode.el
@@ -0,0 +1,11 @@
+;;; angularjs-mode.el --- Angular
+;;; Commentary:
+;;; Code:
+(use-package angular-mode
+  :ensure t
+  )
+
+(use-package angular-snippets
+  :ensure t
+  )
+;;; angularjs-mode.el ends here
--- init.el
+++ init.el
@@ -19,9 +19,9 @@
 
 (load-user-file "general.el")
 (load-user-file "global-key.el")
-
 (load-user-file "php-mode.el")
 (load-user-file "web-mode.el")
+(load-user-file "angularjs-mode.el")
 
 (unless package-archive-contents
   (package-refresh-contents))
@@ -99,7 +99,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (use-package))))
+ '(package-selected-packages (quote (angularjs-mode use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
