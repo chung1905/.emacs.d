7323ed043ebd1c65feea4a1dd68b68c086181dc1
chung <chung1905@gmail.com>
2019-09-05 21:30:28 +0700

    Java

---
+++ els.d/java.el
@@ -0,0 +1,20 @@
+;;; java.el --- Java
+;;; Commentary:
+;;; Code:
+(use-package cc-mode :ensure t)
+(use-package lsp-mode :ensure t)
+(use-package hydra :ensure t)
+(use-package company-lsp :ensure t)
+(use-package lsp-ui :ensure t)
+(use-package lsp-java :ensure t :after lsp
+  :config (add-hook 'java-mode-hook 'lsp))
+
+(use-package dap-mode
+  :ensure t :after lsp-mode
+  :config
+  (dap-mode t)
+  (dap-ui-mode t))
+
+(use-package dap-java :after (lsp-java))
+
+;;; java.el ends here
