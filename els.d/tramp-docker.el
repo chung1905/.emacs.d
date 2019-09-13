0b106ab8ccd79be438cd4f4d87f25477f4f29921
Nguyen Duc Chung <nguyenducchung@sutunam.com>
2019-09-13 15:46:03 +0700

    Tramp & Docker

---
+++ els.d/tramp-docker.el
@@ -0,0 +1,11 @@
+;;; tramp-docker.el --- Angular
+;;; Commentary:
+;;; Code:
+(use-package docker-tramp
+  :ensure t
+  )
+
+(use-package counsel-tramp
+  :ensure t
+  )
+;;; tramp-docker.el ends here
