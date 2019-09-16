bc5893fc9467e0079abdf54059e847ee6b3b4e77
Nguyen Duc Chung <nguyenducchung@sutunam.com>
2019-09-16 08:29:07 +0700

    Fix cmake-mode

---
+++ els.d/cmake.el
@@ -0,0 +1,7 @@
+;;; cmake.el --- cmake
+;;; Commentary:
+;;; Code:
+(use-package cmake-mode
+  :ensure t
+  )
+;;; cmake.el ends here
--- init.el
+++ init.el
@@ -38,6 +38,9 @@
 (load-user-file "tramp-docker.el")
 (load-user-file "cmake.el")
 
+(use-package magit
+  :ensure t)
+
 (use-package diff-hl
   :ensure t
   :config
@@ -136,7 +139,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (cmake-mode use-package))))
+ '(package-selected-packages (quote (use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
