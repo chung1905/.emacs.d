9dac2f486a4d39ca8bcd0d84a7fafa71079cb585
chung <me@chungn.com>
2021-04-15 16:34:21 +0700

    Fix phpactor path (again)

--- els.d/php-mode.el
+++ els.d/php-mode.el
@@ -17,6 +17,16 @@
 
 (use-package lsp-treemacs :ensure t :commands lsp-treemacs-errors-list)
 
-(use-package lsp-php :ensure t :after lsp)
+(use-package lsp-php :after lsp)
+
+(defun lsp-php-get-composer-dir ()
+  "Get composer home directory if possible."
+  (if (executable-find "composer")
+      (replace-regexp-in-string "\n$" "" (shell-command-to-string "composer config --global home"))
+    "~/.composer"))
+
+(setq lsp-php-composer-dir (lsp-php-get-composer-dir))
+
+(setq lsp-phpactor-path (f-join lsp-php-composer-dir "vendor/bin/phpactor"))
 
 ;;; php-mode.el ends here
